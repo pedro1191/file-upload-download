1b6c4c0c95ac17c15ead1cbf9618109f3087a177
pedro1191 <pedro_almeida1191@hotmail.com>
2018-08-28 11:16:35 -0300

    all methods updated to work with files

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -26,127 +26,82 @@
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
         $data = [
-          'title' => trim($_POST['title']),
-          'body' => trim($_POST['body']),
+          'file' => isset($_FILES['file']) ? $_FILES['file'] : null,
           'user_id' => $_SESSION['user_id'],
-          'title_err' => '',
-          'body_err' => ''
+          'file_err' => ''
         ];
 
         // Validate data
-        if(empty($data['title'])){
-          $data['title_err'] = 'Please enter title';
-        }
-        if(empty($data['body'])){
-          $data['body_err'] = 'Please enter body text';
-        }
+        if($data['file'] == null || ($data['file']['size'] == 0 && ($data['file']['error'] == UPLOAD_ERR_OK || $data['file']['error'] == UPLOAD_ERR_NO_FILE))){
+          $data['file_err'] = 'File is mandatory';
+        } elseif ($data['file']['error'] != 0) { // Anything else than UPLOAD_ERR_OK or UPLOAD_ERR_NO_FILE...
+      	  $data['file_err'] = 'An error occurred trying to send the file. Try again';
+      	}
 
         // Make sure no errors
-        if(empty($data['title_err']) && empty($data['body_err'])){
+        if(empty($data['file_err'])){
           // Validated
           if($this->postModel->addPost($data)){
-            flash('post_message', 'Post Added');
+            flash('post_message', 'File Added');
             redirect('posts');
           } else {
-            die('Something went wrong');
+            flash('post_message', 'Oops! Something went wrong trying to add the file', 'alert alert-danger');
+            redirect('posts/add');
           }
         } else {
           // Load view with errors
           $this->view('posts/add', $data);
         }
-
       } else {
         $data = [
-          'title' => '',
-          'body' => ''
+          'file' => null
         ];
   
         $this->view('posts/add', $data);
       }
     }
 
-    public function edit($id){
+    public function show(){
       if($_SERVER['REQUEST_METHOD'] == 'POST'){
         // Sanitize POST array
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-        $data = [
-          'id' => $id,
-          'title' => trim($_POST['title']),
-          'body' => trim($_POST['body']),
-          'user_id' => $_SESSION['user_id'],
-          'title_err' => '',
-          'body_err' => ''
-        ];
-
-        // Validate data
-        if(empty($data['title'])){
-          $data['title_err'] = 'Please enter title';
-        }
-        if(empty($data['body'])){
-          $data['body_err'] = 'Please enter body text';
-        }
-
-        // Make sure no errors
-        if(empty($data['title_err']) && empty($data['body_err'])){
-          // Validated
-          if($this->postModel->updatePost($data)){
-            flash('post_message', 'Post Updated');
-            redirect('posts');
-          } else {
-            die('Something went wrong');
-          }
-        } else {
-          // Load view with errors
-          $this->view('posts/edit', $data);
-        }
-
-      } else {
-        // Get existing post from model
+        $id =  isset($_POST['id']) ? $_POST['id'] : '';
         $post = $this->postModel->getPostById($id);
 
-        // Check for owner
-        if($post->user_id != $_SESSION['user_id']){
-          redirect('posts');
-        }
-
         $data = [
-          'id' => $id,
-          'title' => $post->title,
-          'body' => $post->body
+          'post' => $post
         ];
-  
-        $this->view('posts/edit', $data);
+      } else {
+        $data = [
+          'post' => null
+        ];
       }
-    }
-
-    public function show($id){
-      $post = $this->postModel->getPostById($id);
-      $user = $this->userModel->getUserById($post->user_id);
-
-      $data = [
-        'post' => $post,
-        'user' => $user
-      ];
-
       $this->view('posts/show', $data);
     }
 
-    public function delete($id){
+    public function delete(){
       if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitize POST array
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $id = isset($_POST['id']) ? $_POST['id'] : '';
         // Get existing post from model
         $post = $this->postModel->getPostById($id);
-        
-        // Check for owner
-        if($post->user_id != $_SESSION['user_id']){
-          redirect('posts');
-        }
 
-        if($this->postModel->deletePost($id)){
-          flash('post_message', 'Post Removed');
-          redirect('posts');
+        $error = false;
+
+        if($post != null) {
+          if($this->postModel->deletePost($id)){
+            flash('post_message', 'File Removed');
+            redirect('posts');
+          } else {
+            $error = true;
+          }
         } else {
-          die('Something went wrong');
+          $error = true;
+        }
+        if ($error) {
+          flash('post_message', 'Oops! Something went wrong trying to delete the file', 'alert alert-danger');
+          redirect('posts/show');
         }
       } else {
         redirect('posts');
