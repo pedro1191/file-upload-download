a7d647518f7cc48dd608da49283a3b9888e0b83b
pedro1191 <pedro_almeida1191@hotmail.com>
2018-08-28 10:55:02 -0300

    user index page updated to work with files

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -2,22 +2,30 @@
   <?php flash('post_message'); ?>
   <div class="row mb-3">
     <div class="col-md-6">
-      <h1>Posts</h1>
+      <h1>My Files</h1>
     </div>
     <div class="col-md-6">
-      <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">
-        <i class="fa fa-pencil"></i> Add Post
+      <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-success pull-right">
+        <i class="fa fa-upload"></i> Upload
       </a>
     </div>
   </div>
   <?php foreach($data['posts'] as $post) : ?>
     <div class="card card-body mb-3">
-      <h4 class="card-title"><?php echo $post->title; ?></h4>
-      <div class="bg-light p-2 mb-3">
-        Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+      <div class="row">
+	      <div class="col-md">
+          <h4 class="mt-0 mb-1"><?php echo $post->getFilename(); ?></h4>
+          <p class="mb-2">Modified for the last time on <?php echo date('m/d/Y H:i:s', $post->getMTime()); ?></p>
+        </div>
+	      <div class="col-md">
+          <form action="<?php echo URLROOT; ?>/posts/download" method="post">
+            <button type="submit" value="<?php echo $post->getFilename(); ?>" name="id" class="btn btn-light pull-right"><i class="fa fa-download"></i> Download</button>
+          </form>
+	        <form action="<?php echo URLROOT; ?>/posts/show" method="post">
+            <button type="submit" value="<?php echo $post->getFilename(); ?>" name="id" class="btn btn-secondary pull-right mr-2">Delete</button>
+          </form>
+	      </div>
       </div>
-      <p class="card-text"><?php echo $post->body; ?></p>
-      <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
     </div>
   <?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
