a7184e288c1480b2a4b940aac38a3279340420cf
pedro1191 <pedro_almeida1191@hotmail.com>
2018-08-28 12:15:24 -0300

    log messages added

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -4,23 +4,39 @@
    * Loads the models and views
    */
   class Controller {
+    protected $logger;
+
     // Load model
     public function model($model){
-      // Require model file
-      require_once '../app/models/' . $model . '.php';
+      $this->logger = new Logger(__FILE__);
+      $this->logger->debug(__METHOD__ . " started");
 
-      // Instatiate model
-      return new $model();
+      // Require model file
+      if(file_exists('../app/models/' . $model . '.php')){
+        $this->logger->debug(__METHOD__ . " model $model was found");
+        require_once '../app/models/' . $model . '.php';
+        // Instantiate model
+        return new $model();
+      } else {
+        // Model does not exist
+        $this->logger->error(__METHOD__ . " model $model was not found");
+        die('Oops! Model was not found');
+      }
     }
 
     // Load view
     public function view($view, $data = []){
+      $this->logger = new Logger(__FILE__);
+      $this->logger->debug(__METHOD__ . " started");
+
       // Check for view file
       if(file_exists('../app/views/' . $view . '.php')){
+        $this->logger->debug(__METHOD__ . " view $view was found");
         require_once '../app/views/' . $view . '.php';
       } else {
         // View does not exist
-        die('View does not exist');
+        $this->logger->error(__METHOD__ . " view $view was not found");
+        die('Oops! View was not found');
       }
     }
   }
