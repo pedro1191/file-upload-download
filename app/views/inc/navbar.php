6339c2216eebcce44d6be73d156ad1052b39aa8a
pedro1191 <pedro_almeida1191@hotmail.com>
2018-08-29 16:27:16 -0300

    logo added to index page

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
   <div class="container">
-      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><img class="" alt="<?php echo SITENAME; ?>" src="<?php echo URLROOT; ?>/img/logoazul.png"/></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
